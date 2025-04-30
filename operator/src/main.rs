0ef7a798cb621f1545b9961f8139ccdea1281e5e
Oliver Marcusson <github@faithless.se>
2025-05-01 00:11:47 +0200

    WIP. Removed the obsolete function handle_command

--- operator/src/main.rs
+++ operator/src/main.rs
@@ -46,68 +46,6 @@ impl Server {
     }
 }
 
-async fn handle_command(stream: &mut TcpStream, input: Vec<&str>) -> anyhow::Result<()> {
-    let config = bincode::config::standard();
-    let command = input[0];
-
-    match command {
-        "echo" if input.len() != 2 => println!("Usage: echo <message>"),
-        "echo" => {
-            let message = Message::Echo {
-                payload: Some(input[1].to_string()),
-            };
-            let encoded = bincode::encode_to_vec(&message, config)?;
-            println!("[*] Echoing {}", input[1]);
-            let _ = stream.write_all(&encoded).await;
-        }
-
-        "listener" => {
-            if input.len() != 4 && input.len() != 3 {
-                println!("Usage: listener add/remove");
-                println!("Add: listener add <name> <type>");
-                println!("Types: tcp");
-                println!("\nRemove: listener remove <name>");
-                return Ok(());
-            }
-
-            if input[1] == "add" {
-                let name = input[2];
-                match input[3] {
-                    "tcp" => {
-                        let mut addr = String::new();
-                        print!("Addr (ip:port): ");
-                        io::stdout().flush().unwrap();
-                        io::stdin()
-                            .read_line(&mut addr)
-                            .expect("Failed to read input");
-
-                        let message = Message::Listener {
-                            action: ListenerAction::Add {
-                                name: name.to_string(),
-                                listener_type: ListenerType::Tcp {
-                                    addr: addr.trim().to_string(),
-                                },
-                            },
-                        };
-                        let encoded = bincode::encode_to_vec(&message, config)?;
-                        let _ = stream.write_all(&encoded).await;
-                        println!("[*] Tasked server to add listener '{}'", name);
-                    }
-                    _ => {
-                        println!("Error: Invalid listener type.");
-                        return Ok(());
-                    }
-                }
-            }
-        }
-
-        _ => {
-            todo!();
-        }
-    }
-    Ok(())
-}
-
 fn read_command() -> anyhow::Result<Vec<String>> {
     let mut input = String::new();
     print!("> ");
